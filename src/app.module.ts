5ce8b4e6118002437905b96b5ddc87ab3334f7dc
BamlakAbebayehu <bamlakab@gmail.com>
2024-02-26 17:52:39 +0300

    four commit

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
 import { BookmarkModule } from './bookmark/bookmark.module';
+import { GatewayModule } from './gateway/gateway.module';
 
 
 @Module({
-  imports: [AuthModule, UserModule, BookmarkModule],
+  imports: [AuthModule, UserModule, BookmarkModule, GatewayModule],
 })
 export class AppModule {}
